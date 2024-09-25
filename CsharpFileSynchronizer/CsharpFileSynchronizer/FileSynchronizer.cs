ae14cbe4b8f805080ed15d7bd69d5196c2728c49
TomasM <tomas.bk148@gmail.com>
2024-09-25 21:45:36 +0200

    Added method for calculate md5 hash

--- CsharpFileSynchronizer/CsharpFileSynchronizer/FileSynchronizer.cs
+++ CsharpFileSynchronizer/CsharpFileSynchronizer/FileSynchronizer.cs
@@ -3,24 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace CsharpFileSynchronizer
 {
     public class FileSynchronizer
     {
-        private string source;
-        private string backup;
-        private string log_file;
+        private string _source;
+        private string _backup;
+        private string _log_file;
 
         public FileSynchronizer() 
-        { 
-                        
-            
+        {
+            SetupLogging();
+
+
         }  
         
-        private void setup_logging()
+        private void SetupLogging()
         {
 
         }
+
+        public string CalculateMD5(string filePath)
+        {
+            // Open the file as a FileStream
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    // Compute the MD5 hash
+                    byte[] hashBytes = md5.ComputeHash(stream);
+
+                    // Convert the byte array to a hex string
+                    return BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
     }
 }
