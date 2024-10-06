59830b7d6dfba16201e6f4e8b706d5b913cb7c2c
TomasM <tomas.bk148@gmail.com>
2024-10-06 15:05:43 +0200

    Updated main to call fileSynchronizer

--- CsharpFileSynchronizer/CsharpFileSynchronizer/Program.cs
+++ CsharpFileSynchronizer/CsharpFileSynchronizer/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,15 @@ namespace CsharpFileSynchronizer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hellow This is File Synchronizer");
 
+            Logger logger = new Logger("app.log");
+
+            FileSynchronizer fileSynchronizer = new FileSynchronizer(logger.LoggerInstance, args[0], args[1]);
+            fileSynchronizer.StartSync(1);
+
+            // Dispose the Serilog logger when done
+            Log.CloseAndFlush();
         }
     }
 }
